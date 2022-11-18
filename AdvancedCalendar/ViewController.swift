625a43c22673944c736fa0eb3bc78026a04ab559
kaia <kaiagaoqy@gmail.com>
2022-11-17 21:23:08 -0500

    add dependency

---
+++ AdvancedCalendar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AdvancedCalendar
+//
+//  Created by Kaia Gao on 11/17/22.
+//
+
+import UIKit
+import JZCalendarWeekView
+
+class ViewController: UIViewController {
+    
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    
+    }
+    
+    
+
+
+}
+
